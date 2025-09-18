8dd1cae26c2d4a1aa1f0b72bf16539173c52b10b
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-09-18 15:07:57 -0300

    feat: implement Ierc20

--- contracts/weth9.sol
+++ contracts/weth9.sol
@@ -1,7 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.29;
 
-contract WETH9 {
+import "./interfaces/IERC20L2.sol";
+
+contract WETH9 is IERC20L2 {
+    // IERC20L2 implementation
+    address public L1_TOKEN = address(0);
+    address public constant BRIDGE = 0x000000000000000000000000000000FFff;
+    constructor(address l1Addr) {
+        L1_TOKEN = l1Addr;
+    }
+
+    modifier onlyBridge() {
+        require(msg.sender == BRIDGE, "WETH9: not authorized");
+        _;
+    }
+
+    function l1Address() external view returns (address) {
+        return L1_TOKEN;
+    }
+
+    function crosschainMint(address to, uint256 amount) external onlyBridge {
+        balanceOf[to] += amount;
+        emit Transfer(address(0), to, amount);
+    }
+
+    function crosschainBurn(address from, uint256 amount) external onlyBridge {
+        require(balanceOf[from] >= amount);
+        balanceOf[from] -= amount;
+        emit Transfer(from, address(0), amount);
+    }
+
+    // WETH9 implementation
     string public name = "Wrapped Ether";
     string public symbol = "WETH";
     uint8 public decimals = 18;
