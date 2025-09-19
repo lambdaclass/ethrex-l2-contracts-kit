818df1d8bbbc813e57c1227899b7b0156209d375
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-09-19 12:01:02 -0300

    add deploy script

--- scripts/deployer.rs
+++ scripts/deployer.rs
@@ -1,26 +1,26 @@
-use std::{
-    path::{Path, PathBuf},
-    str::FromStr,
-};
+use std::path::{Path, PathBuf};
 
 use clap::{Parser, arg, command};
+use ethereum_types::{H160, H256};
 use ethrex_l2_rpc::signer::{LocalSigner, Signer};
 use ethrex_rpc::EthClient;
 use ethrex_sdk::{create2_deploy_from_path, git_clone};
 use eyre::Result;
+use hex::FromHexError;
+use reqwest::Url;
 use secp256k1::SecretKey;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(long, default_value = "http://localhost:1729")]
-    rpc_url: String,
+    rpc_url: Url,
+    #[arg(long, value_parser = parse_private_key)]
+    private_key: SecretKey,
     #[arg(long)]
-    private_key: String,
+    l1_token: H160,
     #[arg(long)]
-    l1_token: String,
-    #[arg(long)]
-    salt: String,
+    salt: H256,
 }
 
 async fn compile_and_deploy(args: Args) -> Result<()> {
@@ -50,24 +50,35 @@ async fn compile_and_deploy(args: Args) -> Result<()> {
     )
     .expect("Failed to compile contract");
     let mut constructor_args = vec![0u8; 12];
-    let l1_token_address = hex::decode(&args.l1_token).expect("Failed to decode L1 token address");
-    constructor_args.extend_from_slice(&l1_token_address);
+    let l1_token_address = args.l1_token.as_bytes();
+    constructor_args.extend_from_slice(l1_token_address);
 
-    let deployer = Signer::Local(LocalSigner::new(SecretKey::from_str(&args.private_key)?));
-    let eth_client = EthClient::new("http://localhost:1729")?;
+    let deployer = Signer::Local(LocalSigner::new(args.private_key));
+    let eth_client = EthClient::new(args.rpc_url.as_str())?;
     let contract_path = PathBuf::from("solc_out/WETH9.bin");
-    let salt = hex::decode(&args.salt).expect("Failed to decode salt");
+    let salt = &args.salt.as_bytes();
     let (tx_hash, deployed_address) = create2_deploy_from_path(
         &constructor_args,
         &contract_path,
         &deployer,
-        &salt,
+        salt,
         &eth_client,
     )
     .await?;
     println!("deployed with tx hash {tx_hash:?}");
     println!("deployed at address {deployed_address:?}");
     Ok(())
+}
+
+pub fn parse_hex(s: &str) -> eyre::Result<Vec<u8>, FromHexError> {
+    match s.strip_prefix("0x") {
+        Some(s) => hex::decode(s),
+        None => hex::decode(s),
+    }
+}
+
+pub fn parse_private_key(s: &str) -> eyre::Result<SecretKey> {
+    Ok(SecretKey::from_slice(&parse_hex(s)?)?)
 }
 
 #[tokio::main]
