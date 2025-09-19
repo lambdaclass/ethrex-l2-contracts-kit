a4cb8e07c3899680bf374c5da24243113042046e
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-09-19 11:49:35 -0300

    add arguments

--- scripts/deployer.rs
+++ scripts/deployer.rs
@@ -3,14 +3,27 @@ use std::{
     str::FromStr,
 };
 
+use clap::{Parser, arg, command};
 use ethrex_l2_rpc::signer::{LocalSigner, Signer};
 use ethrex_rpc::EthClient;
 use ethrex_sdk::{create2_deploy_from_path, git_clone};
 use eyre::Result;
 use secp256k1::SecretKey;
 
-async fn compile_contracts() -> Result<()> {
-    // Logic to compile contracts
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long, default_value = "http://localhost:1729")]
+    rpc_url: String,
+    #[arg(long)]
+    private_key: String,
+    #[arg(long)]
+    l1_token: String,
+    #[arg(long)]
+    salt: String,
+}
+
+async fn compile_and_deploy(args: Args) -> Result<()> {
     git_clone(
         "https://github.com/OpenZeppelin/openzeppelin-contracts.git",
         "lib",
@@ -37,17 +50,14 @@ async fn compile_contracts() -> Result<()> {
     )
     .expect("Failed to compile contract");
     let mut constructor_args = vec![0u8; 12];
-    let l1_token_address = hex::decode("178a38b8f3f1b1fa632c95c60519bb77bd041074")
-        .expect("Failed to decode L1 token address");
+    let l1_token_address = hex::decode(&args.l1_token).expect("Failed to decode L1 token address");
     constructor_args.extend_from_slice(&l1_token_address);
 
-    let deployer = Signer::Local(LocalSigner::new(SecretKey::from_str(
-        "bcdf20249abf0ed6d944c0288fad489e33f66b3960d9e6229c1cd214ed3bbe31",
-    )?));
+    let deployer = Signer::Local(LocalSigner::new(SecretKey::from_str(&args.private_key)?));
     let eth_client = EthClient::new("http://localhost:1729")?;
     let contract_path = PathBuf::from("solc_out/WETH9.bin");
-    let salt = [0u8; 32];
-    let a = create2_deploy_from_path(
+    let salt = hex::decode(&args.salt).expect("Failed to decode salt");
+    let (tx_hash, deployed_address) = create2_deploy_from_path(
         &constructor_args,
         &contract_path,
         &deployer,
@@ -55,13 +65,14 @@ async fn compile_contracts() -> Result<()> {
         &eth_client,
     )
     .await?;
-    dbg!(a.1);
-    dbg!(a.0);
+    println!("deployed with tx hash {tx_hash:?}");
+    println!("deployed at address {deployed_address:?}");
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    compile_contracts().await?;
+    let args = Args::parse();
+    compile_and_deploy(args).await?;
     Ok(())
 }
