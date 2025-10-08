5b10f114fefbad27dfd8b0baef0ef6067d449711
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-10-08 17:09:17 -0300

    use oz erc20 impl

--- contracts/WETH9.sol
+++ contracts/WETH9.sol
@@ -2,12 +2,13 @@
 pragma solidity =0.8.29;
 
 import "./interfaces/IERC20L2.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract WETH9 is IERC20L2 {
+contract WETH9 is IERC20L2, ERC20 {
     // IERC20L2 implementation
     address public L1_TOKEN = address(0);
     address public constant BRIDGE = address(0xffff);
-    constructor(address l1Addr) {
+    constructor(address l1Addr) ERC20("Wrapped Ether", "WETH") {
         L1_TOKEN = l1Addr;
     }
 
@@ -32,64 +33,19 @@ contract WETH9 is IERC20L2 {
         revert("WETH9: burns are not allowed");
     }
 
-    // WETH9 implementation
-    string public name = "Wrapped Ether";
-    string public symbol = "WETH";
-    uint8 public decimals = 18;
-
     event Deposit(address indexed dst, uint wad);
     event Withdrawal(address indexed src, uint wad);
-
-    mapping(address => uint) public balanceOf;
-    mapping(address => mapping(address => uint)) public allowance;
 
     receive() external payable {
         deposit();
     }
     function deposit() public payable {
-        balanceOf[msg.sender] += msg.value;
+        _mint(msg.sender, msg.value);
         emit Deposit(msg.sender, msg.value);
     }
     function withdraw(uint wad) public {
-        require(balanceOf[msg.sender] >= wad);
-        balanceOf[msg.sender] -= wad;
+        _burn(msg.sender, wad);
         payable(msg.sender).transfer(wad);
         emit Withdrawal(msg.sender, wad);
-    }
-
-    function totalSupply() public view returns (uint) {
-        return address(this).balance;
-    }
-
-    function approve(address guy, uint wad) public returns (bool) {
-        allowance[msg.sender][guy] = wad;
-        emit Approval(msg.sender, guy, wad);
-        return true;
-    }
-
-    function transfer(address dst, uint wad) public returns (bool) {
-        return transferFrom(msg.sender, dst, wad);
-    }
-
-    function transferFrom(
-        address src,
-        address dst,
-        uint wad
-    ) public returns (bool) {
-        require(balanceOf[src] >= wad);
-
-        if (
-            src != msg.sender && allowance[src][msg.sender] != type(uint256).max
-        ) {
-            require(allowance[src][msg.sender] >= wad);
-            allowance[src][msg.sender] -= wad;
-        }
-
-        balanceOf[src] -= wad;
-        balanceOf[dst] += wad;
-
-        emit Transfer(src, dst, wad);
-
-        return true;
     }
 }
